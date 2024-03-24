88886c77d6e2540fe8ce3f3281e297af95ba4828
Hamza_Sheikh <160158151+Hamza-Sheikh-02@users.noreply.github.com>
2024-03-24 15:17:57 +0500

    Day1

---
+++ Day1/app.js
@@ -0,0 +1,10 @@
+"use strict";
+// Question 1: Install Node.js, TypeScript and VS Code on your computer/Laptop.
+// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+let personName1 = "Hamza";
+console.log(`Hello ${personName1}, would you like to learn some TypeScript today?`);
+// Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName2 = "Hamza";
+console.log(personName2.toLowerCase());
+console.log(personName2.toUpperCase());
+console.log(personName2.charAt(0).toUpperCase() + personName2.slice(1));
