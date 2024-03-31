e0afa9f008b62f05ce82c7ccd34141fa147cc002
Hamza_Sheikh <160158151+Hamza-Sheikh-02@users.noreply.github.com>
2024-03-31 13:06:11 +0500

    Day30

---
+++ Day30/app.js
@@ -0,0 +1,18 @@
+// Question 88: Rounding to the Nearest Integer: Write a function that takes a decimal number as input and returns the number rounded to the nearest integer.
+function roundToNearestInteger(num) {
+    return Math.round(num);
+}
+console.log(roundToNearestInteger(6.8));
+console.log(roundToNearestInteger(6.3));
+// Question 89: Converting Strings to Numbers: Create a function that takes a string representing a number (like "123") and converts it into an actual number type.
+function convertStringToNumber(str) {
+    return parseFloat(str);
+}
+console.log(convertStringToNumber("182.81"));
+console.log(convertStringToNumber("69"));
+// Question 90: Checking if a Value is NaN: Demonstrate how to check if a variable's value is NaN (Not a Number) and return a boolean result.
+function isValueNaN(value) {
+    return isNaN(value);
+}
+console.log(isValueNaN("hamza")); // Outputs: true because "hamza" is not a number
+console.log(isValueNaN(592)); // Outputs: false because 592 is a number
