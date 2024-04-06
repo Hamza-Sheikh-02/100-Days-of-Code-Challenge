80b5d181d577eccf035658216b8b6270063a9be4
Hamza_Sheikh <160158151+Hamza-Sheikh-02@users.noreply.github.com>
2024-04-06 10:51:10 +0500

    Day36

---
+++ Day36/app.js
@@ -0,0 +1,18 @@
+// Question 106: Determine if a given year is a leap year using comparison operators.
+function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+console.log(isLeapYear(2024));
+console.log(isLeapYear(2022));
+// Question 107: Write a condition using logical operators that checks if a number is divisible by both 2 and 3.
+function isDivisibleByTwoAndThree(number) {
+    return number % 2 === 0 && number % 3 === 0;
+}
+console.log(isDivisibleByTwoAndThree(18));
+console.log(isDivisibleByTwoAndThree(20));
+// Question 108: Compare two strings to check if they are identical, ignoring case sensitivity.
+function areStringsEqualIgnoreCase(str1, str2) {
+    return str1.toLowerCase() === str2.toLowerCase();
+}
+console.log(areStringsEqualIgnoreCase("hamza", "Hamza"));
+console.log(areStringsEqualIgnoreCase("to", "Too"));
